232f0717bed72620bc59ed8ccbb81ab263c20b30
Atefeh <at.bakhshi1373@gmail.com>
2022-12-04 21:48:43 +0330

    feat: display date

--- weatherApp/details.js
+++ weatherApp/details.js
@@ -1,12 +1,13 @@
 const city_name = location.search.split("city=")[1];
-
+// get elements fron html file
 const cityName = document.getElementById("city-name");
+const date = document.getElementById("date");
 const degree = document.getElementById("now");
 const minDegree = document.getElementById("min");
 const maxDegree = document.getElementById("max");
 const weatherCondithin = document.getElementById("weather");
 const weatherIcon = document.getElementById("weather-icon");
-
+// get information from api by city name
 async function gettingCityName(city) {
   try {
     let res = await fetch(
@@ -19,7 +20,7 @@ async function gettingCityName(city) {
     console.log(error);
   }
 }
-
+// get information from api by id
 async function result(id) {
   try {
     let res = await fetch(
@@ -32,7 +33,19 @@ async function result(id) {
     console.log(error);
   }
 }
+// display date
+function today(d){
+    let year=d.getFullYear();
+    let month=d.getMonth();
+    let day=d.getDate();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    let second = d.getSeconds();
+    date.innerText=`${year}:${month}:${day}  /  ${hour}:${minute}:${second}`
+}
+today(new Date())
 
+// display results in page
 function showResult(data) {
   cityName.innerText = data.name;
   degree.innerText = data.main.temp;
@@ -60,7 +73,7 @@ function showResult(data) {
     weatherCondithin.innerText = weather;
   }
 }
-
+// display hourly degree
 function showHourly(data) {
   const [first, second, third, four, five, six, others] = data.list;
   let kelvin = [first, second, third, four, five, six, others];
@@ -72,11 +85,13 @@ function showHourly(data) {
     houres.push(+item.dt_txt.split(" ")[1].split(":")[0]);
   });
   
+
   let xValues = houres;
   let yValues = celcius;
   console.log(yValues)
   let minY = yValues[0];
   let maxY=yValues[0];
+//   find min and max values of degree
   yValues.forEach((item) => {
     if(item<minY){
         minY=item;
@@ -85,7 +100,7 @@ function showHourly(data) {
         maxY=item;
     }
   });
-
+// create linear chart
   new Chart("myChart", {
     type: "line",
     data: {
