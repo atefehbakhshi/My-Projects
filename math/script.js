bf1f17a9161c47f619a768b7d63e588dfed63bb4
Atefeh <at.bakhshi1373@gmail.com>
2022-12-07 21:54:43 +0330

    feat:create pages

--- math/script.js
+++ math/script.js
@@ -1,1 +1,10 @@
 'use strict'
+const calculator = document.getElementById('calculator');
+const shapes = document.getElementById('shapes');
+const charts = document.getElementById('charts');
+// calculator.addEventListener('click',()=>{
+//     window.location.href="calculator.html";
+// })
+// shapes.addEventListener('click',()=>{
+//     window.location.href="shapes/shapes.html";
+// })
